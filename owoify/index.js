7a7e7857d97b8962213ac65fdae4d627cf789f42
Alyxia Sother <lexisoth2005@gmail.com>
2021-08-17 19:19:32 +0200

    [owoify] Added plugin

---
+++ owoify/index.js
@@ -0,0 +1,31 @@
+import {webpackModules} from "@cumcord/modules";
+
+let unpatch;
+
+function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+export default {
+    onLoad() {
+        unpatch = cumcord.patcher.patch("sendMessage", webpackModules.findByProps("sendMessage"), (args) => {
+            if (args[1].content.startsWith("!owo")) {
+                const msg = args[1].content.replace("!owo", "");
+
+                const faces = ["owo", "UwU", ">w<", "^w^"];
+                const owoified = msg
+                    .replace(/[rl]/g, "w")
+                    .replace(/[RL]/g, "W")
+                    .replace(/ove/g, "uv")
+                    .replace(/n/g, "ny")
+                    .replace(/N/g, "NY")
+                    .replace(/\!/g, ` ${random(faces)} `);
+
+                args[1].content = owoified;
+            }
+        });
+    },
+    onUnload() {
+        unpatch();
+    }
+};
