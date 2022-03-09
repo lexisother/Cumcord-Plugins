17febcf06123ad4d4d71c0100ffc5aa5f0aa5fb0
alethéia <34745171+xirreal@users.noreply.github.com>
2022-03-09 10:30:20 +0100

    [modding-discover] Patch title, disable caching

--- modding-discover/index.js
+++ modding-discover/index.js
@@ -5,21 +5,23 @@ import { before, after } from '@cumcord/patcher';
 const GuildDiscoverySearchBar = findByDisplayName('GuildDiscoverySearchBar', false);
 // const discoveryCard = findByDisplayName('GuildDiscoveryCard', false);
 const guildResources = findByProps('getGuildDiscoverySplashURL');
+const discoveryCategories = findByProps('getDiscoveryCategories')
 
 let fetchSuccess;
 let addCategory;
 let addGuilds;
 let removeBar;
 let patchGetDiscoverySplash;
+let patchTitleInDiscoverPage;
 
 export default {
   async onLoad() {
-    const data = await (await fetch('https://public.alyxia.dev/guilddb.json')).json();
+    const data = await (await fetch('https://public.alyxia.dev/guilddb.json', {cache: "no-cache"} )).json();
     findByProps('isDispatching').dispatch({ type: 'GUILD_DISCOVERY_POPULAR_FETCH_SUCCESS' });
     fetchSuccess = FluxDispatcher._orderedActionHandlers.GUILD_DISCOVERY_POPULAR_FETCH_SUCCESS[0];
     addCategory = after(
       'getDiscoveryCategories',
-      findByProps('getDiscoveryCategories'),
+      discoveryCategories,
       (_, res) => {
         res.push({ categoryId: 99, name: 'Client Modding' });
         return res;
@@ -37,9 +39,11 @@ export default {
       }
       return res;
     });
+    patchTitleInDiscoverPage = after('getCategoryName', discoveryCategories, (args, res) => {
+      return (args[0] == 99) ? 'Client Modding' : res;
+    });
     removeBar = after('default', GuildDiscoverySearchBar, (args, res) => {
-      if(args[0].currentCategoryId == 99) return null;
-      return res;
+      return (args[0].currentCategoryId == 99) ? null : res;
     });
     // cardPatch = before('default', discoveryCard, (args) => {
     //   console.log("Penis?");
@@ -51,5 +55,6 @@ export default {
     addGuilds();
     removeBar();
     patchGetDiscoverySplash();
+    patchTitleInDiscoverPage();
   },
 };
