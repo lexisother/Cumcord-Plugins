82ca4d68cb06e8b1a4f809bff74a0e49c6b50e25
cloud <62519659+lem6ns@users.noreply.github.com>
2022-03-27 19:21:27 -0600

    Fix classes

--- hideblockedmessages/index.js
+++ hideblockedmessages/index.js
@@ -2,7 +2,7 @@ let css;
 
 export default {
     onLoad() {
-        css = cumcord.patcher.injectCSS(`.groupStart-23k01U:not(.message-2qnXI6){display: none;}`);
+        css = cumcord.patcher.injectCSS(`.groupStart-3Mlgv1:not(.message-2CShn3){display: none;}`);
     },
     onUnload() {
         css();
