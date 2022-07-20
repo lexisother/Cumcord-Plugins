c539dd7b9a0618e5db60fe49d3989eb23772f621
echo <91651232+exhq@users.noreply.github.com>
2022-07-20 23:29:03 +0430

    actually good uwuifier (#3)
    Co-authored-by: Alyxia Sother <lexisoth2005@gmail.com>

--- owoify/index.js
+++ owoify/index.js
@@ -1,32 +1,103 @@
-import {webpackModules} from "@cumcord/modules";
-
-let unpatch;
-
-function random(array) {
-    return array[Math.floor(Math.random() * array.length)];
+import { webpackModules } from "@cumcord/modules";
+function randomarray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function random(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+let output = "";
+let x = "";
+let ismodified = false;
 export default {
-    onLoad() {
-        unpatch = cumcord.patcher.after("sendMessage", webpackModules.findByProps("sendMessage"), (args) => {
-            if (args[1].content.startsWith("!owo")) {
-                const msg = args[1].content.replace("!owo", "");
+  onLoad() {
+    const endings = [
+      "owo",
+      "UwU",
+      ">w<",
+      "^w^",
+      "●w●",
+      "☆w☆",
+      "𝗨𝘄𝗨",
+      "(´꒳`)",
+      "♥(。U ω U。)",
+      "(˘ε˘)",
+      "( ˘ᴗ˘ )",
+      "(*ฅ́˘ฅ̀*)",
+      "*screams*",
+      "*twearks*",
+      "*sweats*",
+    ];
+    const removeCommand = cumcord.commands.addCommand({
+      name: "uwuify",
 
-                const faces = ["owo", "UwU", ">w<", "^w^"];
-                const owoified = msg
-                    .replace(/[rl]/g, "w")
-                    .replace(/[RL]/g, "W")
-                    .replace(/ove/g, "uv")
-                    .replace(/n/g, "ny")
-                    .replace(/N/g, "NY")
-                    .replace(/\!/g, ` ${random(faces)} `);
+      description: "uwuify your amazing string",
+      args: [
+        {
+          name: "string",
+          description: "the thing thats getting uwufified",
+          type: "string",
+          required: true,
+        },
+      ],
 
-                args[1].content = owoified;
-            }
-            return args;
-        });
-    },
-    onUnload() {
-        unpatch();
-    }
+      handler: (ctx, send) => {
+        output = "";
+        var input = ctx.args.string.split(" ");
+        for (let i = 0; i < input.length; i++) {
+          ismodified = false;
+          //yes its stutter but fuck you im too lazy to fix it
+          let chancesutter = random(1, 5);
+          x = input[i]
+            .toLowerCase()
+            .replace("r", "w")
+            .replace("l", "w")
+            .replace("ne", "nye")
+            .replace("no", "nyo")
+            .replace("ni", "nyi");
+          if (x.endsWith("!")) {
+            x += shuffle(["1", "!", "!", "1"]).join("");
+            ismodified = true;
+          }
+          if (x.endsWith("?")) {
+            x += shuffle(["/", "?", "?"]).join("");
+            ismodified = true;
+          }
+          if (x.endsWith(".") && (random(1, 3) == 1) | 2) {
+            x += " " + randomarray(endings);
+            ismodified = true;
+          }
+          if (chancesutter == 1) {
+            x = `${x[0]}-${x[0]}-${x}`;
+            ismodified = true;
+          }
+
+          if (!ismodified && !x.includes("owo") && !x.includes("uwu")) {
+            x = x.replace("o", "owo");
+          }
+          if (!ismodified && !x.includes("owo") && !x.includes("uwu")) {
+            x = x.replace("u", "uwu");
+          }
+          output += x + " ";
+        }
+
+        return output;
+      },
+    });
+  },
+  onUnload() {},
 };
