3c209fbb9e8a8987df0f38d5a4c80c6e400fd588
Beef <beefers@riseup.net>
2022-08-02 15:09:00 +0100

    [DevMode] Update to match Discord's updates
    The new method is entirely different to the old one, and is seemingly impossible to unpatch. As a compromise, the plugin will now display a confirmation message informing the user that they need to reload Discord when the plugin is unloaded.

--- devmode/index.js
+++ devmode/index.js
@@ -1,8 +1,20 @@
 import { findByProps } from "@cumcord/modules/webpack";
+import { instead } from "@cumcord/patcher";
+import { showConfirmationModal } from "@cumcord/ui/modals";
 
-const devStore = findByProps("isDeveloper");
+const userMod = findByProps("getUsers");
+const CONNECTION_OPEN = findByProps("isDeveloper")._dispatcher._orderedActionHandlers["CONNECTION_OPEN"];
+try { CONNECTION_OPEN.find(x => x.name === "ExperimentStore").actionHandler({ user: { ...userMod.getCurrentUser(), flags: 1 }, type: "CONNECTION_OPEN" }); } catch (e) {};
 
-Object.defineProperty(devStore, "isDeveloper", { configurable: true, value: true });
+let gcUserPatch = instead("getCurrentUser", userMod, (args, originalFunc) => { return { ...originalFunc(), hasFlag: () => true }});
+CONNECTION_OPEN.find(x => x.name === "DeveloperExperimentStore").actionHandler();
+gcUserPatch();
 
-// i tested, yes this leaves the original intact
-export const onUnload = () => delete devStore.isDeveloper;
+export const onUnload = () => {
+    showConfirmationModal({
+        header: "DevMode",
+        content: "For developer features to be fully disabled, you need to reload Discord.",
+        confirmText: "Reload",
+        type: "Danger"
+    }).then(() => document.location.reload());
+}
