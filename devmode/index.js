964b95ffc25cf1604975af4a6af98b33ca492168
Beef <beefers@riseup.net>
2022-08-02 19:45:28 +0100

    [DevMode] Fix breaking with other plugins

--- devmode/index.js
+++ devmode/index.js
@@ -3,11 +3,11 @@ import { instead } from "@cumcord/patcher";
 import { showConfirmationModal } from "@cumcord/ui/modals";
 
 const userMod = findByProps("getUsers");
-const CONNECTION_OPEN = findByProps("isDeveloper")._dispatcher._orderedActionHandlers["CONNECTION_OPEN"];
-try { CONNECTION_OPEN.find(x => x.name === "ExperimentStore").actionHandler({ user: { ...userMod.getCurrentUser(), flags: 1 }, type: "CONNECTION_OPEN" }); } catch (e) {};
+const nodes = Object.values(findByProps("isDeveloper")._dispatcher._dependencyGraph.nodes);
+nodes.find(x => x.name === "ExperimentStore").actionHandler["CONNECTION_OPEN"]({ user: { flags: 1 }, type: "CONNECTION_OPEN", experiments: [] });
 
-let gcUserPatch = instead("getCurrentUser", userMod, (args, originalFunc) => { return { ...originalFunc(), hasFlag: () => true }});
-CONNECTION_OPEN.find(x => x.name === "DeveloperExperimentStore").actionHandler();
+let gcUserPatch = instead("getCurrentUser", userMod, () => { return { hasFlag: () => true }});
+nodes.find(x => x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
 gcUserPatch();
 
 export const onUnload = () => {
