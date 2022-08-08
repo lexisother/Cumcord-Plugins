666db622e48d5051a4d6feb4c0305a53e7792fd1
Beef <beefers@riseup.net>
2022-08-08 17:05:26 +0100

    [DevMode] Update to match Discord's updates (#4)
    * [DevMode] Update to match Discord's updates
    
    The new method is entirely different to the old one, and is seemingly impossible to unpatch. As a compromise, the plugin will now display a confirmation message informing the user that they need to reload Discord when the plugin is unloaded.
    
    * [DevMode] Fix breaking with other plugins
    
    * [DevMode] Move to OVERLAY_INITIALIZE to fix issues

--- devmode/index.js
+++ devmode/index.js
@@ -1,8 +1,20 @@
 import { findByProps } from "@cumcord/modules/webpack";
+import { instead } from "@cumcord/patcher";
+import { showConfirmationModal } from "@cumcord/ui/modals";
 
-const devStore = findByProps("isDeveloper");
+const userMod = findByProps("getUsers");
+const nodes = Object.values(findByProps("isDeveloper")._dispatcher._dependencyGraph.nodes);
+try { nodes.find(x => x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } }); } catch {};
 
-Object.defineProperty(devStore, "isDeveloper", { configurable: true, value: true });
+let gcUserPatch = instead("getCurrentUser", userMod, () => { return { hasFlag: () => true }});
+nodes.find(x => x.name === "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
+gcUserPatch();
 
-// i tested, yes this leaves the original intact
-export const onUnload = () => delete devStore.isDeveloper;
+export const onUnload = () => {
+    showConfirmationModal({
+        header: "DevMode",
+        content: "For developer features to be fully disabled, you need to reload Discord.",
+        confirmText: "Reload",
+        type: "Danger"
+    }).then(() => document.location.reload());
+}
